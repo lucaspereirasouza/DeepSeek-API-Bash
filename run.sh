8eba889d8195503b952ef42d9c06924ea44aa009
lucaspereirasouza <lucaspereiradesouza2163@outlook.com>
2025-01-29 22:01:52 -0300

    git fix (.sh): Fixed dual initialization, fixed directory problem

--- run.sh
+++ run.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
 DIR=$(dirname "$0")
-PY_= "$DIR" ""./venv/bin/python"
+PY_="$DIR/venv/bin/python"
 
 echo "Starting Flask..."
-
-PY_ main.py &
+$PY_ main.py &
 
 FLASK_PID=$!
 
-echo "Starting Client..."
-PY_ terminalUI.py
-
 echo "Finishing Flask..."
 kill $FLASK_PID
-
-deactivate
